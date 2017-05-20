e37d459cf12d22c2e1d83d04842a7372bf4a51d7
ProgressBG-WWW-Courses <ProgressBG.WWW.Courses+gh@gmail.com>
2017-05-20 18:47:29 +0300

    WIP: wordsCounter-HW

--- pages/themes/RegEx/HW/wordsCounter.js
+++ pages/themes/RegEx/HW/wordsCounter.js
@@ -1,100 +1,190 @@
 'use strict';
-
-var init = function(){
-	console.log(`init starts`);
-	var nodes = {};
-
-	function getNodes(){
-		console.log(`getNodes starts`);
-		if ( Object.keys(nodes).lenght == undefined ) {
-			console.log(`Nodes are empty`);
-			nodes.textarea = document.querySelector('#wordsCounter  textarea');
-			nodes.select = document.querySelector('#wordsCounter  select');
-			nodes.button = document.querySelector('#wordsCounter  button');
+var NumSorter = function(){
+	// methods
+	keysByValuesDesc = function(obj){
+		 return Object.keys(obj).sort((a,b)=>obj[b]-obj[a]);
+	};
+	keysByValuesAsc = function(obj){
+		 return Object.keys(obj).sort((a,b)=>obj[a]-obj[b]);
+	};
+	keysByKeysDesc = function(obj){
+		 return Object.keys(obj).sort((a,b)=> b-a);
+	};
+	// interface
+	return{
+		keysByKeysDesc: keysByKeysDesc,
+		keysByValuesAsc: keysByValuesAsc,
+		keysByValuesDesc: keysByValuesDesc,
+	}
+}
+var Matcher = function(text, pattern) {
+	// data
+	var pattern  = pattern || /\b[a-z]{3,}?\b/g,
+		text  = text || "",
+		matched =  {};
+	// getters/setters
+	var getText = ()=>text;
+	var getMatched = ()=>matched;
+	// methods
+	var init = function(text){
+		// if we change text => genrate matches
+		console.log(`init matcher`);
+		try{
+			if (this.text ){
+				throw "We already have text!"
+			}else{
+				this.text = text;
+				this.matched = this.text.match(pattern);
+				// TODO:  throw error on empty mathched?
+			}
+		}catch(e){
+			console.log(`ERROR: ${e}`);
 		}
+	};
+	// interface
+	return{
+		getText:getText,
+		getMatched:getMatched,
+		init:init,
+	}
+}
+var Counter  = function(){
+	// data
+
+	// methods
+	var countAllWords= function(){
+		console.log(`countAllWords() started`);
+		var wordsCount = 0;
+	};
+	var countSeparateWords= function(wordsObj){
+		console.log(`countAllWords starts`);
+
+		var wordList = {};
+
+		wordsObj.forEach( (word)=>wordList[word]++ || (wordList[word] = 1) );
+
+		// sort the keys array
+		var sorted = numSorter.keysByValuesDesc(wordList);
+
+		// display results:
+		sorted.forEach(key=>{
+
+			console.log(`${key}: ${wordList[key]}`)
+		});
+	}
+	// interface
+	return{
+		countAllWords: countAllWords,
+		countSeparateWords: countSeparateWords
+	}
+}
+var Presenter = function(output, obj){
+	// data
+
+	// methods
+	var asTable = function(){
+		var table = document.createElement('table');
+		var i = 0; // row index
+
+		obj.forEach(key=>{
+			var j = 0; // column index
+			var row = table.insertRow(i++);
+
+			var cell = row.insertCell(j++);
+			cell.innerHTML = key;
+			cell = row.insertCell(j++);
+			cell.innerHTML = obj[key];
+		})
+
+		output.appendChild(table);
+	}
+	// interface
+	return{
+		asTable: asTable,
+	}
+}
+
+var main = function(){
+	function getNodes(){
+		var nodes = {};
+		console.log(`getNodes starts`);
+
+		nodes.textarea = document.querySelector('#wordsCounter  textarea');
+		nodes.select = document.querySelector('#wordsCounter  select');
+		nodes.button = document.querySelector('#wordsCounter  button');
+		nodes.results = document.querySelector('#results');
 
 		return nodes;
 	};
 
-	function atachEvents(nodes, actions){
-		console.log(`atachEvents starts`);
-		var nodes = getNodes();
-
-		document.addEventListener("DOMContentLoaded", ()=>{
-			// nodes.select.onchange = getAction;
-			nodes.button.onclick = function(actions){
-				return	performAction(actions);
-			};
-			console.dir(this);
-		}, false)
+	function atachEventListener(node, eventName, listener){
+		console.log(`atachEventListener starts`);
+		node.addEventListener( eventName, listener, false );
 	}
 
-	function performAction(actions){
-		console.log(`performAction started`);
-		console.dir(actions);
-		var action = nodes.select.value;
-
-		if (  action == undefined || action == ""){
-			alert("Choose an action first!");
-		}else{
-			// if ( typeof action == "function")
-		}
-	}
-
-	function getAction(event){
-		if(!event.target.value) {
-			console.log(`Please, select an action!`);
-		}else{
-			console.log(`action: ${event.target.value}`);
-		}
+	function onDOMContentLoaded(){
+		console.log(`DOMContentLoaded fired!`);
+		// attach events to elements
+		atachEventListener(nodes.button, "click", performSelectedAction);
 	}
 
 	function getSelectedOption(select){
+		console.log(`getSelectedOption() started`);
 		try{
-			console.dir(select);
-			let selected  = select[select.selectedIndex].value;
-			console.log(`selected: ${selected}`);
-
+			return select[select.selectedIndex].value;
 		}catch(e){
-			console.warn(`Error: ${e}`);
+			console.warn(`Error: can not getSelectedOption: ${e}`);
 		}
 	}
 
+	// function getSelectedAction(event){
+	// 	if(!event.target.value) {
+	// 		console.log(`Please, select an action!`);
+	// 	}else{
+	// 		console.log(`action: ${event.target.value}`);
+	// 	}
+	// }
 
-	return {
-		// nodes: nodes,
-		atachEvents: atachEvents,
-		performAction: performAction,
-		getSelectedOption:getSelectedOption,
-	}
-}();
+	function performSelectedAction(){
+		console.log(`performSelectedAction started`);
+		console.dir(counter);
 
-var actions = {
-	countAllWords: function(){
-			var text = `Far out in the uncharted backwaters of the unfashionable end of the western spiral arm of the Galaxy lies a small unregarded yellow sun. Orbiting this at a distance of roughly ninety-two million miles is an utterly insignificant little blue green planet whose ape- descended life forms are so amazingly primitive that they still think digital watches are a pretty neat idea. This planet has or rather had a problem, which was this: most of the people on it were unhappy for pretty much of the time. Many solutions were suggested for this problem, but most of these were largely concerned with the movements of small green pieces of paper, which is odd because on the whole it wasn't the small green pieces of paper that were unhappy. And so the problem remained; lots of the people were mean, and most of them were miserable, even the ones with digital watches. Many were increasingly of the opinion that they'd all made a big mistake in coming down from the trees in the first place. And some said that even the trees had been a bad move, and that no one should ever have left the oceans. And then, one Thursday, nearly two thousand years after one man had been nailed to a tree for saying how great it would be to be nice to people for a change, one girl sitting on her own in a small cafe in Rickmansworth suddenly realized what it was that had been going wrong all this time, and she finally knew how the world could be made a good and happy place. This time it was right, it would work, and no one would have to get nailed to anything. Sadly, however, before she could get to a phone to tell anyone about it, a terribly stupid catastrophe occurred, and the idea was lost forever. This is not her story. But it is the story of that terrible stupid catastrophe and some of its consequences. It is also the story of a book, a book called The Hitch Hiker's Guide to the Galaxy not an Earth book, never published on Earth, and until the terrible catastrophe occurred, never seen or heard of by any Earthman. Nevertheless, a wholly remarkable book. In fact it was probably the most remarkable book ever to come out of the great publishing houses of Ursa Minor of which no Earthman had ever heard either. Not only is it a wholly remarkable book, it is also a highly successful one more popular than the Celestial Home Care Omnibus, better selling than Fifty More Things to do in Zero Gravity, and more controversial than Oolon Colluphid's trilogy of philosophical blockbusters Where God Went Wrong, Some More of God's Greatest Mistakes and Who is this God Person Anyway? In many of the more relaxed civilizations on the Outer Eastern Rim of the Galaxy`;
+		var action = getSelectedOption(nodes.select);
 
-			var wordPattern = /\b[a-z]{3,}?\b/g;
-
-			var wordList = {};
-			if (text.match(wordPattern)){
-				text.match(wordPattern)
-					.forEach(function(word){
-						wordList[word]++ || (wordList[word] = 1);
-					});
+		// if there is no text processed so far => get and match the new text:
+		var text = matcher.getText();
+		var matched;
+		if ( !text){
+			var newText = nodes.textarea.value;
+			if ( newText ){
+				matcher.init(newText);
+				matched = matched.getMatched();
+			}else{
+				alert("Plese, enter text to be processed!")
+				throw "Error: no text";
 			}
+		}
 
-			// списъкът, сортиран по брой срещания:
-			var sortedByValues = Object.keys(wordList)
-				.sort((a,b)=>wordList[b]-wordList[a]);
+		// perform action
+		if (  action == undefined || action == ""){
+			alert("Choose an action first!");
+		}else{
+			console.log(`action: ${action}`);
+			if ( typeof counter[action] == 'function'){
+				counter[action](matcher.matched);
+			}else{
+				console.warn(`Action ${action} is not a function!`);
+			}
+		}
+	}
 
-			sortedByValues.forEach(key=>console.log(`${key}: ${wordList[key]}`));
-	},
-	countSeparateWords: function(){}
-}
+	// main
+	var nodes = getNodes();
+	var matcher = Matcher();
+	var counter = Counter();
+	atachEventListener(document, "DOMContentLoaded", onDOMContentLoaded);
 
-// ~~~MAIN~~~
-init.atachEvents(actions);
-
+}();
 
 
 

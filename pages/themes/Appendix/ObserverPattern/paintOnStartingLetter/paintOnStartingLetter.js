d76f58732e8390880ba8949c6f5e451beba31411
ProgressBG-WWW-Courses <ProgressBG.WWW.Courses+gh@gmail.com>
2017-06-19 22:52:01 +0300

    Lab17: 19.06.2017 DONE: Observer Pattern Next: Ajax && APIs consume TODO: files

--- pages/themes/Appendix/ObserverPattern/paintOnStartingLetter/paintOnStartingLetter.js
+++ pages/themes/Appendix/ObserverPattern/paintOnStartingLetter/paintOnStartingLetter.js
@@ -15,21 +15,13 @@ augment( nodes.subject, new Subject() );
 for( let o of nodes.observers){
 	// extend observers with Observer methods
 	augment( o, new Observer());
-	o.notify = function(letter){
-		var firstLetter = o.innerHTML.charAt(0);
 
-		if( firstLetter.toLowerCase() == letter.toLowerCase() ){
-			o.style.color = '#F00';
-		}else{
-			o.style.color = '#999';
-		}
-	};
 
 	// Register the observers to nodes.subject
 	nodes.subject.registerObserver(o);
 }
 
-nodes.subject.addEventListener('input', (e)=>{
+nodes.subject.addEventListener('change', (e)=>{
 	var letter = e.target.value;
 
 	e.target.notifyObservers(letter);

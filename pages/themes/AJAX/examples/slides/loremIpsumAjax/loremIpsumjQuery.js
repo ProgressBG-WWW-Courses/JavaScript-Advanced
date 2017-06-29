d575c3b41c9bbb2c50f35c89efa106209538a54f
ProgressBG-WWW-Courses <ProgressBG.WWW.Courses+gh@gmail.com>
2017-06-29 11:01:42 +0300

    Lab20: 28.06.2017 DONE: AJAX with Examples NEXT: Angular TODO: ...

--- pages/themes/AJAX/examples/slides/loremIpsumAjax/loremIpsumjQuery.js
+++ pages/themes/AJAX/examples/slides/loremIpsumAjax/loremIpsumjQuery.js
@@ -1,10 +1,12 @@
 var dataURL = 'loremIpsum.txt';
+// https://raw.githubusercontent.com/ProgressBG-WWW-Courses/JavaScript-Advanced/gh-pages/downloads/products.json
 
-var getByJQuery = function(){
+var getByJQuery = function(cb){
 	$(".getDataBtn").click(function(){
 		$.get(dataURL, function(data, status){
 			// do something with data:
-			$(".output").html(data);
+			// $(".output").html(data);
+			cb(data);
 		});
 	});
 }
@@ -15,5 +17,13 @@ var loadByJQuery = function(){
 	});
 }
 
-loadByJQuery();
+var productData;
+var parseData = function(str) {
+	// var res = JSON.parse(str);
+	render(str);
+}
+var render = function(data){
+	$(".output").html(data);
+}
+getByJQuery(render);
 
